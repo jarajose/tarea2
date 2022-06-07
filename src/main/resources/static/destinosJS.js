cf9672e7f4f021c66297fad164b2b884163a6e3d
JOSE DANIEL RODRIGUEZ JARA <jrodriguezj1@ucenfotec.ac.cr>
2022-06-07 01:09:59 -0600

    Primer commit del proyecto Tarea2

---
+++ src/main/resources/static/destinosJS.js
@@ -0,0 +1,22 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/javascript.js to edit this template
+ */
+const panels = document.querySelectorAll('.panel')
+
+panels.forEach((panel) => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
+
+
+function removeActiveClasses() {
+    panels.forEach((panel => {
+        panel.classList.remove('active')
+    })
+
+    )
+}
+
---
+++ src/test/java/tarea3/tarea3/Tarea3ApplicationTests.java
@@ -0,0 +1,13 @@
+package tarea3.tarea3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tarea3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
